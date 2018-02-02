a98bdd83a49d86a3140da8871d43d88f6ffd9a0f
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2018-02-02 22:51:38 +0000

    add mobile viewport, production configuration and update memory usage

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sbt universal:package-zip-tarball
 docker build -t login-example .
-docker tag login-example scapig/login-example:0.1
-docker push scapig/login-example:0.1
+docker tag login-example scapig/login-example
+docker push scapig/login-example
