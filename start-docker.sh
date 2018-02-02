3405cf9ede54c2fd3734810d8d3942ed1d9dd085
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2018-02-02 23:45:35 +0000

    add configurable config file

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT=$(find . -type f -name login-example)
 rm -f login-example*/RUNNING_PID
-exec $SCRIPT -Dhttp.port=9040 -J-Xms16M -J-Xmx64m
+exec $SCRIPT -Dhttp.port=9040 -Dconfig.resource=$configFile -J-Xms16M -J-Xmx64m
